9e2c2a1963726a1caa45764d8cbe4623f926b9a4
SavIt2022 <developer1@sav-ita.com>
2022-11-28 11:13:14 +0300

    colors

---
+++ accounts/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+from .forms import CreateUserForm
+appname="my_app"
+
+def login_student(request):
+	return render(request, 'students_login.html')
+
+def login_school_admin(request):
+	return render(request, 'school_admin_login.html')
+
+def login_admin(request):
+	return render(request, 'admin_login.html')
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('login')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('home')
+			
+
+		context = {'form':form}
+		return render(request, 'register.html', context)
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('login')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('login')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'login.html', context)
+
+
+def logoutUser(request):
+    if request.method == 'POST':
+        return redirect('login')
+   
